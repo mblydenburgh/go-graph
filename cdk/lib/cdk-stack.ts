38097384b330d57070c8b5b6c36d0bc4a4294eb0
Michael Blydenburgh <mike.blydenburgh@gmail.com>
2024-04-14 12:40:55 -0400

    update function provisioning

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,40 +1,46 @@
-import * as cdk from 'aws-cdk-lib';
-import * as lambda from 'aws-cdk-lib/aws-lambda';
-import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as eventSource from "aws-cdk-lib/aws-lambda-event-sources";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
+import { Construct } from "constructs";
+import path = require("path");
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const table = new dynamodb.Table(this, 'Table', {
-      tableName: 'go-graph-table',
+    const table = new dynamodb.Table(this, "Table", {
+      tableName: "go-graph-table",
       partitionKey: {
-        name: 'PK',
+        name: "PK",
         type: dynamodb.AttributeType.STRING
       },
       sortKey: {
-        name: 'SK',
+        name: "SK",
         type: dynamodb.AttributeType.STRING
       },
       removalPolicy: cdk.RemovalPolicy.DESTROY
     })
     table.addGlobalSecondaryIndex({
-          indexName: 'GSI1',
+          indexName: "GSI1",
           partitionKey: {
-            name: 'GSI1PK',
+            name: "GSI1PK",
             type: dynamodb.AttributeType.STRING
           },
           sortKey: {
-            name: 'GSI1SK',
+            name: "GSI1SK",
             type: dynamodb.AttributeType.STRING
           }
     })
 
     // new docker based go lambda
-    const goLambda = new lambda.DockerImageFunction(this, 'GoLambda', {
-      code: lambda.DockerImageCode.fromImageAsset('lambda/go'),
+    const goLambda = new lambda.DockerImageFunction(this, "GoLambda", {
+      functionName: "go-graph-lambda",
+      code: lambda.DockerImageCode.fromImageAsset(path.resolve(__dirname, "../../")),
+      architecture: lambda.Architecture.ARM_64,
+      memorySize: 256,
     });
+    goLambda.addEventSource(new eventSource.ApiEventSource("ANY", "/"))
 
   }
 }
